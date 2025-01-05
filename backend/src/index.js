95eb8513186adf2e6c141d1616711f8a37262b61
Rico <ricodevelop25@gmail.com>
2025-01-04 22:52:07 -0500

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,6 +40,11 @@ app.use("/api/songs", songRoutes);
 app.use("/api/album", albumRoutes);
 app.use("/api/stat", statRoutes);
 
+//error handler
+app.use((err,req,res,next) => {
+  res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+})
+
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
     connectDB();
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ import { clerkClient } from "@clerk/express";
 
 export const protectRoute = async (req, res, next) => {
   if (!req.auth.userId){
-    return res.status(401).json({ message: "Unauthorizedd - you mus be logged in"});
+    return res.status(401).json({ message: "Unauthorized - you must be logged in"});
   }
   
   next();
